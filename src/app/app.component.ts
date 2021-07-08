44f8cb51aaec0f706820f59fac4ac5098004450c
Melanie <contact.mriche@gmail.com>
2021-07-08 14:23:25 +0200

    using :decrement and adding some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,42 @@ import { RouterOutlet } from '@angular/router';
         query(':enter, :leave', [
           style({
             position: 'absolute',
+            overflow: 'hidden',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }),
+        ], { optional: true }),
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-50px)'
+            })) 
+          ], { optional: true }),
+          query(':enter', [
+            style({
+              opacity: 0,
+              transform: 'translateX(50px)'
+            }),
+            animate('200ms 120ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            })) 
+          ], { optional: true })
+        ])
+
+      ]),
+
+      // DECREMENT TRANSITION
+      transition(':decrement', [
+        style({
+          position: 'relative',
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
             top: 0,
             left: 0,
             width: '100%',
@@ -23,25 +59,20 @@ import { RouterOutlet } from '@angular/router';
             overflow: 'hidden'
           }),
         ], { optional: true }),
-        // query(':enter', [
-        //  style({
-        //    opacity: 0
-        //  }),
-        //], { optional: true }),
 
         group([
           query(':leave', [
             animate('200ms ease-in', style({
               opacity: 0,
-              transform: 'translateX(-80px)'
+              transform: 'translateX(50px)'
             })) 
           ], { optional: true }),
           query(':enter', [
             style({
               opacity: 0,
-              transform: 'translateX(80px)'
+              transform: 'translateX(-50px)'
             }),
-            animate(200, style({
+            animate('200ms 120ms ease-out' , style({
               opacity: 1,
               transform: 'translateX(0)'
             })) 
