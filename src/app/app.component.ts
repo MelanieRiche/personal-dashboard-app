c67e93fe548892abcd9f3e9d633442b368ca2705
Melanie <contact.mriche@gmail.com>
2021-07-08 11:40:29 +0200

    Trying to work on routing transitions animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            background: 'wheat',
+            display: 'block',
+            height: '100%'
+          }),
+          animate(1000, style({
+            background: '*',
+            height: 'auto'
+          }))
+        ], { optional: true }),
+
+        style({
+          background: 'blue'
+        }),
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-dashboard-app';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.snapshot && outlet.activatedRouteData.snapshot.url;
+  }
+
 }
