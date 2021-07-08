e44e3e724df42c46f8d4bc5e864db3277464d108
Melanie <contact.mriche@gmail.com>
2021-07-08 16:49:15 +0200

    Using addNote to add our note into the array

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Note } from '../shared/note.model';
+import { NoteService } from '../shared/note.service';
 
 @Component({
   selector: 'app-add-note',
@@ -8,14 +10,19 @@ import { NgForm } from '@angular/forms';
 })
 export class AddNoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
   }
 
   onFormSubmit(form: NgForm) {
     // alert("Form has been submitted!")
-    console.log(form)
+    // console.log(form.value)
+    const note = new Note(form.value.title, form.value.content)
+    console.log(note)
+
+    // Adding the note to the array on note.service.ts
+    this.noteService.addNote(note)
   }
 
 }
