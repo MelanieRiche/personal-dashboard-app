97ba9008d4e0a44bd67540796b15741e0c2953c9
Melanie <contact.mriche@gmail.com>
2021-07-08 16:24:01 +0200

    Generating each Note Card dynamically based on the data in the Note.Service

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Note } from '../shared/note.model';
 
 @Component({
   selector: 'app-note-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.scss']
 })
 export class NoteCardComponent implements OnInit {
+
+  @Input() note!: Note
 
   constructor() { }
 
