918590e4e7c5718ddde169718724f5cd46510343
Melanie <contact.mriche@gmail.com>
2021-07-14 17:27:28 +0200

    Fixing errors on Ts version - Learning new syntax + creating the edit todo view

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { TodoService } from '../shared/todo.service';
+import { Todo } from '../shared/todo.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-todo',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTodoComponent implements OnInit {
 
-  constructor() { }
+  todo!: Todo
+
+  constructor(private route: ActivatedRoute, private todoService: TodoService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      // alert(todoId)
+      const todoId = paramMap.get('id')!
+      this.todo = this.todoService.getTodo(todoId)!
+    })
   }
 
+  onformsubmit(form: NgForm) {
+    this.todoService.updateTodo(this.todo.id, form.value)
+    this.router.navigateByUrl("/todos")
+  }
+
+  onFormSubmit(form: NgForm) {
+  }
 }
